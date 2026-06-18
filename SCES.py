991a1e49db60290ec9b26351ff86fb14b0702e58
Beatriz Dias <beatriz.d@edu.senai.br>
2026-06-18 09:13:04 -0300

    Fazendo tudo do começo já que sumiu meu git antigo e o segundo não aceitava commit

---
+++ SCES.py
@@ -0,0 +1,8 @@
+# [ID do Produto (numérico), Nome do Produto (texto), Quantidade em Estoque (numérico), Localização
+produtos = [
+    [5864, "Cabos", 350, "Pratileira B-02-12"],
+    [7856, "Parafuso", 400, "Pratileira C-06-24"],
+    [9680, "Fitas", 180, "Pratileira H-05-36"],
+    [2851, "Prego", 536, "Pratileira V-07-48"],
+    [4683, "Porca", 620, "Pratileira L-01-60"]
+]
